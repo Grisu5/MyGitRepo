772ea60ce2720898e4818fa11c7f98b9ce5099ff
Elhassan Elnasir <elnasir555@yahoo.com>
2023-03-12 14:07:45 +0400

    New lessons added

---
+++ Week 7/products.js
@@ -0,0 +1,49 @@
+products = [
+    {   
+        id: 111,
+        title: "Afk Cat",
+        description: "A nice looking, cute cat " + "for those looking for some love in their lives.",
+        price: 500,
+        image: "img/cat.jpg",
+        availableInventory: 6,
+        rating: 2,},
+        {   
+            id: 112,
+            title: "ddd Cat",
+            description: "A nice looking, cute cat " + "for those looking for some love in their lives.",
+            price: 700,
+            image: "img/cat.jpg",
+            availableInventory: 10,
+            rating: 4,
+        },
+
+        {
+            id: 113,
+            title: "fff Cat",
+            description: "A nice looking, cute cat " + "for those looking for some love in their lives.",
+            price: 200,
+            image: "img/cat.jpg",
+            availableInventory: 3,
+            rating: 3,
+        },
+        {
+            id: 114,
+            title: "ggg Cat",
+            description: "A nice looking, cute cat " + "for those looking for some love in their lives.",
+            price: 100,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+            rating: 3,
+        },
+      
+        {
+            id: 115,
+            title: "ccc Cat",
+            description: "A nice looking, cute cat " + "for those looking for some love in their lives.",
+            price: 50,
+            image: "img/cat.jpg",
+            availableInventory: 7,
+            rating: 1,
+        },
+     
+]
