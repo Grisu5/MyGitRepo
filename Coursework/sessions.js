447aff430b60878b477f646695a0a2bdea9b2cd6
Elhassan Elnasir <elnasir555@yahoo.com>
2023-03-12 18:09:48 +0400

    sessins json fle

---
+++ Coursework/sessions.js
@@ -0,0 +1,100 @@
+sessions = [
+    {   
+        id: 111,
+        title: "Dance",
+        price: 500,
+        icon: "fa-solid fa-music",
+        availableInventory: 5,
+        location: "Omdurman"
+    },
+
+    {   
+            id: 112,
+            title: "Music",
+            price: 700,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+            location: "Khartoum"
+
+    },
+
+    {
+            id: 113,
+            title: "Photography",
+            price: 400,
+            image: "img/cat.jpg",
+            availableInventory: 3,
+            location: "Bahri"
+
+    },
+
+    {
+            id: 114,
+            title: "Football",
+            price: 800,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+            location: "Khartoum"
+
+    },
+      
+    {
+            id: 115,
+            title: "Film",
+            price: 700,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+            location: "Bahri"
+
+    },
+
+    {
+            id: 116,
+            title: "Programming",
+            price: 100,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+            location: "Omdurman"
+
+    },
+
+    {
+            id: 117,
+            title: "Graphics Design",
+            price: 300,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+            location: "Bahri"
+
+    },
+
+    {
+            id: 118,
+            title: "Fashion",
+            price: 400,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+    }, 
+
+    {
+            id: 119,
+            title: "Creative writing",
+            price: 100,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+            location: "Khartoum"
+
+    },
+
+    {
+            id: 110,
+            title: "Mindfullness",
+            price: 50,
+            image: "img/cat.jpg",
+            availableInventory: 5,
+            location: "Omdurman"
+
+    },
+    
+     
+]
